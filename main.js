04872c9d3291245a42560306273a62d7ffda9c0c
KrishnaM03 <96728162+KrishnaM03@users.noreply.github.com>
2022-11-12 14:37:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 
 function preload() {
     img = loadImage('dog_cat.jpg');
@@ -14,18 +15,19 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320)
+    if(status != "") {
+        for(i=0; i < objects.length; i++) {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
+
 
 function modelLoaded() {
     console.log("Model Loaded");
@@ -38,4 +40,5 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
